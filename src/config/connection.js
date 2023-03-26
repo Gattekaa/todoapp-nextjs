756aedb25ad3e9b59ade687c1368d6ec335e6fad
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-03-26 15:36:29 -0300

    url config

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 import { parseCookies } from 'nookies'
-
 const { token } = parseCookies()
 
 const connection = axios.create({
-    baseURL: 'https://todoapp-nextjs-amber.vercel.app/api',
+    baseURL: 'https://gattekatodo.vercel.app/api',
 })
 
 
