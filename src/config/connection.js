7a64478d05fefefb8cd56bc6c9073291d3cae722
Vinicius Gabriel <77711909+Gattekaa@users.noreply.github.com>
2023-03-27 22:40:27 -0300

    Update connection.js

--- src/config/connection.js
+++ src/config/connection.js
@@ -3,7 +3,7 @@ import { parseCookies } from 'nookies'
 const { token } = parseCookies()
 
 const connection = axios.create({
-    baseURL: 'https://https://gattekatodo.vercel.app/api',
+    baseURL: 'https://gattekatodo.vercel.app/api',
 })
 
 
